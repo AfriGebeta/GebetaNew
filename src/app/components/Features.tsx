a938a6f0f62bf7350d869f7e0559b57503eeb203
zubeyranwar <zubeyranwar@gmail.com>
2024-10-11 11:46:53 +0300

    feat: Refactor

--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -1,15 +1,17 @@
 import Container from "@/app/components/Container";
 import Image from "next/image";
 import {features} from "@/app/constants";
+import Link from "next/link";
 
 export default function Features() {
     return (
         <div className="overflow-hidden">
             {
                 features.map((feature, index) => (
-                    <div className="relative mt-[300px]" key={index}>
+                    <div className={`relative ${(index === 0 ) ?"mt-[100px]" : "mt-[300px]"}`} key={index}>
                         <Container>
-                            <div className="flex justify-between mt-[130px] z-50">
+                            <div
+                                className={`flex ${(index + 1) % 2 === 0 ? "flex-row-reverse" : ""} justify-between mt-[130px] z-50`}>
                                 <div>
                                     <h5
                                         className="w-fit px-[30px] py-[15px] bg-[#FFF7E8] rounded-[16px] text-[12px] text-[#FFA500] font-extrabold tracking-20 uppercase">
@@ -17,6 +19,18 @@ export default function Features() {
                                     <h2 className="text-[40px] text-[#1B1E2B] mt-[12px]">{feature.title}</h2>
                                     <p className="max-w-xl text-[20px] text-[#62677F] leading-25 mt-[32px]"><span
                                         className="font-bold">GebetaMaps</span> {feature.description}</p>
+                                    <div className="w-fit flex items-center gap-[8px] group mt-[16px]">
+                                        <Link
+                                            href="/"
+                                            className="text-[14px] text-[#1B1E2B] font-medium">
+                                            Read more</Link>
+                                        <Image
+                                            className="group-hover:translate-x-1 transition-all duration-400"
+                                            src="/assets/right-arrow.svg"
+                                            width={16}
+                                            height={16}
+                                            alt="right arrow icon"/>
+                                    </div>
                                 </div>
                                 <Image
                                     className="z-50"
@@ -29,9 +43,10 @@ export default function Features() {
 
                         {/*    Flavour elements*/}
                         <div
-                            className="absolute left-[70%] -top-[15%] w-[400px] h-[400px] bg-[#FFF6E4] rounded-[48px] z-0"></div>
+                            className={`absolute ${(index + 1) % 2 === 0 ? "left-0 -top-[15%]" : "left-[70%] -top-[15%]"} w-[400px] h-[400px] bg-[#FFF6E4] rounded-[48px] z-0`}></div>
                         <div
-                            className="absolute left-[45%] top-[75%] w-[200px] h-[200px] bg-[#FFF6E4] rounded-[48px] z-0"></div>
+                            className={`absolute ${(index + 1) % 2 === 0 ? "left-[40%] top-[75%]" : "left-[45%] top-[75%]"} w-[200px] h-[200px] bg-[#FFF6E4] rounded-[48px] z-0`}></div>
+
                     </div>
                 ))
             }
