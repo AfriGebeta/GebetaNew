4642c0d4a2fee94b45c871cef5a81642226b8891
zubeyranwar <zubeyranwar@gmail.com>
2024-10-21 10:06:38 +0300

    feat: Add variants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
-import type { Config } from "tailwindcss";
-
 const config: {
   plugins: any[];
   theme: {
     extend: {
       dropShadow: { custom: string };
+      keyframes: { scroll: { "100%": { transform: string }; "0%": { transform: string } } };
+      backgroundImage: { custom: string };
       letterSpacing: { 20: string };
-      lineHeight: { 25: string; 15: string; 60: string; 50: string };
-      colors: { background: string; foreground: string }
+      lineHeight: { 25: string; 15: string; 17: string; 60: string; 50: string };
+      colors: { background: string; foreground: string };
+      animation: { scroll: string }
     }
   };
   content: string[]
@@ -30,13 +31,28 @@ const config: {
         25: "25.2px",
         50: "50.4px",
         60: "60.48px",
-        15:"15.12px"
+        15:"15.12px",
+        17:"17.64px",
       },
       dropShadow: {
         'custom': '0 8px 16px rgba(255, 165, 0, 0.15)',
+      },
+      backgroundImage:{
+        "custom": 'linear-gradient(179.416deg, rgba(255, 255, 255, 0%) 0%, rgba(255, 165, 0, 0%) 30%, rgba(255, 165, 0, 0.3) 100%)',
+      },
+      animation: {
+        'scroll': 'scroll 60s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-256px * 5 - 32px * 5))' },
+        }
       }
     },
   },
   plugins: [],
 };
 export default config;
+
+
