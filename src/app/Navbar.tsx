194edb2a7376d279a736e0db2f0c973fb02ed3ab
zubeyranwar <zubeyranwar@gmail.com>
2024-10-21 10:02:32 +0300

    feat: Style navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,58 +1,164 @@
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
 import Link from "next/link";
 import Container from "@/app/components/Container";
+import {menuItems} from "@/app/constants";
+import {useNavbarLogic} from "@/utils/useNavbarLogic";
+
 export default function Navbar() {
+    const {
+        activeMenu,
+        isScrolled,
+        isMobileMenuOpen,
+        activeMobileSubmenu,
+        handleMouseEnter,
+        handleMouseLeave,
+        toggleMobileMenu,
+        toggleMobileSubmenu,
+    } = useNavbarLogic();
+
     return (
-        <Container>
-            <nav className="flex justify-between items-center mt-[30px]">
-                <div className="flex items-start">
-                    <Image src="/assets/logo.svg" width={30} height={30} alt="logo"/>
-                    <span className='text-[14px] text-[#2E384E] font-semibold tracking-wider'>GebetaMaps</span>
-                </div>
-
-                <div>
-                    <ul className="flex gap-[40px] text-[#2E384E] text-[14px] font-medium">
-                        <li className="flex items-center gap-[6px]">
-                            Products
-                            <Image src="/assets/nav-icon.svg" width="7" height="5" alt="nav drop down icon"/>
-                        </li>
-                        <li className="flex items-center gap-[6px]">
-                            Solutions
-                            <Image src="/assets/nav-icon.svg" width="7" height="5" alt="nav drop down icon"/>
-                        </li>
-                        <li className="flex items-center gap-[6px]">
-                            Developers
-                            <Image src="/assets/nav-icon.svg" width="7" height="5" alt="nav drop down icon"/>
-                        </li>
-                        <li className="flex items-center gap-[6px]">
-                            Company
-                            <Image src="/assets/nav-icon.svg" width="7" height="5" alt="nav drop down icon"/>
-                        </li>
-                        <li>Blog</li>
-                        <li>Pricing</li>
-                    </ul>
-                </div>
-
-                <div className="flex gap-[32px] text-[#2E384E] text-[14px]">
-                    <div className="flex items-center gap-[4px]">
-                        <Image
-                            src="/assets/user.svg"
-                            alt="user icon"
-                            width={24}
-                            height={24}/>
-                        <Link
-                            className="font-medium"
-                            href="/auth/sign-in"
-                        >Sign In</Link>
+        <div
+            className={`fixed top-0 left-0 right-0 z-[1000] bg-white dark:bg-gray-800 transition-shadow duration-300 ${isScrolled ? "shadow-md" : ""}`}
+        >
+            <Container>
+                <nav className="flex justify-between items-center py-4">
+                    <div className="flex items-center">
+                        <Image src="/assets/logo.svg" width={30} height={30} alt="logo" className="mr-2" />
+                        <Link href="/" className="text-[16px] text-[#2E384E] dark:text-white font-semibold tracking-wider">GebetaMaps</Link>
                     </div>
-                    <div className="px-[30px] py-[15px] border border-[#D2C09D] text-[14px] rounded-[8px]">
-                        <Link
-                            className="font-bold"
-                            href="/auth/signup"
-                        >Get Started</Link>
+
+                    <div className="sm:hidden">
+                        <button
+                            className="text-gray-600 dark:text-white focus:outline-none"
+                            onClick={toggleMobileMenu}
+                        >
+                            {isMobileMenuOpen ? (
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            ) : (
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                                </svg>
+                            )}
+                        </button>
                     </div>
+
+                    <div className="hidden sm:flex flex-grow justify-center">
+                        <ul className="flex text-[#2E384E] dark:text-white text-[14px] font-medium">
+                            {menuItems.map((item, index) => (
+                                <li
+                                    key={index}
+                                    className="relative px-4 py-2 cursor-pointer transition-colors duration-200"
+                                    onMouseEnter={() => item.submenu && handleMouseEnter(item.title)}
+                                    onMouseLeave={item.submenu && handleMouseLeave}
+                                >
+                                    <Link href={item.link || "#"} className="flex items-center gap-1">
+                                        {item.title}
+                                        {item.submenu && (
+                                            <svg className="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                                            </svg>
+                                        )}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="hidden sm:flex gap-8">
+                        <div className="flex items-center gap-[4px]">
+                            <Image
+                                className="dark:fill-whitesmoke"
+                                src="/assets/user.svg"
+                                alt="user icon"
+                                width={24}
+                                height={24}/>
+                            <Link
+                                className="font-medium"
+                                href="/auth/sign-in"
+                            >Sign In</Link>
+                        </div>
+                        <div className="px-[30px] py-[15px] transition-all border border-[#D2C09D] hover:border-[#FFA500] hover:text-[#FFA500] hover:bg-[#FFA500]/20 text-[14px] rounded-[8px]">
+                            <Link
+                                className="font-bold"
+                                href="/auth/signup"
+                            >Get Started</Link>
+                        </div>
+                    </div>
+                </nav>
+
+                {isMobileMenuOpen && (
+                    <div className="sm:hidden bg-white dark:bg-gray-800">
+                        <ul className="text-[#2E384E] dark:text-white text-[14px] font-medium">
+                            {menuItems.map((item, index) => (
+                                <li key={index} className="px-4 py-2 border-b">
+                                    <div className="flex justify-between items-center"
+                                         onClick={() => item?.submenu && toggleMobileSubmenu(item.title)}>
+                                        <Link href={item.link || "#"}>{item.title}</Link>
+                                        {item?.submenu && (
+                                            <svg className={`w-4 h-4 transform ${activeMobileSubmenu === item.title ? 'rotate-180' : ''}`} viewBox="0 0 24 24">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                                            </svg>
+                                        )}
+                                    </div>
+                                    {item.submenu && activeMobileSubmenu === item.title && (
+                                        <ul className="pl-4 mt-2 space-y-2">
+                                            {item.submenu.map((subItem, subIndex) => (
+                                                <Link href={subItem.link} key={subIndex} className="text-sm p-2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md">
+                                                    {subItem.title}
+                                                </Link>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="mt-4 space-y-4">
+                            <div className="flex items-center gap-[4px]">
+                                <Image
+                                    className="dark:fill-whitesmoke"
+                                    src="/assets/user.svg"
+                                    alt="user icon"
+                                    width={24}
+                                    height={24}/>
+                                <Link
+                                    className="font-medium"
+                                    href="/auth/sign-in"
+                                >Sign In</Link>
+                            </div>
+                            <div className="w-fit px-[30px] py-[15px] transition-all border border-[#D2C09D] hover:border-[#FFA500] hover:text-[#FFA500] hover:bg-[#FFA500]/20 text-[14px] rounded-[8px]">
+                                <Link
+                                    className="font-bold"
+                                    href="/auth/signup"
+                                >Get Started</Link>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </Container>
+
+            {activeMenu && menuItems.find((item) => item.title === activeMenu)?.submenu && (
+                <div
+                    className="absolute left-0 w-full bg-white dark:bg-gray-800 shadow-lg border-t border-gray-200 dark:border-gray-700"
+                    onMouseEnter={() => handleMouseEnter(activeMenu)}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <Container>
+                        <div className="py-8 grid grid-cols-4 gap-8">
+                            {menuItems.find((item) => item.title === activeMenu).submenu.map((subItem, index) => (
+                                <Link
+                                     href={subItem.link}
+                                     key={index}
+                                     className="block space-y-2 p-3 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md">
+                                    <h3 className="text-[14px] font-semibold">{subItem.title}</h3>
+                                    <p className="text-[12px] text-gray-600 dark:text-gray-400">{subItem.description}</p>
+                                </Link>
+                            ))}
+                        </div>
+                    </Container>
                 </div>
-            </nav>
-        </Container>
-    )
+            )}
+        </div>
+    );
 }
