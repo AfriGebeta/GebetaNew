4fd75fec42441f57ed9917f17215b924451d7124
zubeyranwar <zubeyranwar@gmail.com>
2024-10-21 10:06:03 +0300

    feat: removing class name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
-"use client"
-import Container from "@/app/components/Container";
-import Link from "next/link";
 import VideoCarousel from "@/app/components/VideoCarousel";
-import Image from "next/image";
 import Highlight from "@/app/components/Highlight";
-import Partners from "@/app/components/Partners";
 import Features from "@/app/components/Features";
 import Header from "@/app/components/Header";
+import Pricing from "@/app/components/Pricing";
+import Testimonial from "@/app/components/Testimonal";
+import Partners from "@/app/components/Partners";
+import Showcase from "@/app/components/Showcase";
+import CallToAction from "@/app/components/CallToAction";
 
 export default function Home() {
-    return (
-        <main className="">
-            <Header/>
-            {/*<VideoCarousel/>*/}
-            <Partners/>
-            <div className="h-[1px] bg-[#FFF5E2] mt-[96px]"></div>
-            <Highlight/>
-            <Features/>
 
+    return (
+        <main>
+            <Header/>
+            <VideoCarousel/>
+            <Partners/>
+            <Highlight/>
+            <div className="h-[1px] bg-[#FFF5E2] mt-[96px]"></div>
+            <Features/>
+            <Pricing/>
+            <Testimonial/>
+            <Showcase/>
+            <CallToAction/>
         </main>
     );
 }
