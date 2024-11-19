189954352858bb4905163c0e4ccc48b393a5f6bd
zubeyranwar <zubeyranwar@gmail.com>
2024-11-19 14:37:55 +0300

    feat: Remove disabling selectedplan to let user to buy multiple times

--- src/app/dashboard/plan/page.tsx
+++ src/app/dashboard/plan/page.tsx
@@ -95,8 +95,6 @@ function PlanCard({plan, currentUser, key}) {
     };
 
     const handleUpgrade = () => {
-        if (isPurchased) return;
-
         if (plan.name !== "Custom") {
             buyCredit(currentUser?.token, id)
                 .then(response => {
@@ -152,7 +150,6 @@ function PlanCard({plan, currentUser, key}) {
                     className="w-full py-4 bg-[#FFA500] hover:bg-[#FFA500]/110 text-white transition-all duration-300"
                     variant={isPurchased ? "secondary" : "default"}
                     onClick={handleUpgrade}
-                    disabled={isPurchased}
                 >
                     {getButtonText()}
                 </Button>
