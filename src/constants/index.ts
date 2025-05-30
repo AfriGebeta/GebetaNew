d81c72e58bc0a457d7687d81bffdc2ba7a8c2018
zubeyranwar <zubeyranwar@gmail.com>
2025-05-30 10:36:01 +0300

    feat: Add "/"

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -104,7 +104,7 @@ export const features: Array<Features> = [
         subtitle: "Geocoding",
         description: "makes it effortless to access and integrate detailed local data. Whether for web or mobile platforms, we empower you to customize and include all the data you need, ensuring your users get the most relevant and up-to-date information.",
         image: {
-            source: "assets/geocoding.jpg",
+            source: "/assets/geocoding.jpg",
             alt: "searching places",
             width: 586,
             height: 494
@@ -116,7 +116,7 @@ export const features: Array<Features> = [
         subtitle: "Route Optimization",
         description: "API provides your users with the most optimal routes, making navigation easy. Whether you're developing for web or mobile, we ensure streamlined navigation, helping users save time and reach their destinations faster.",
         image: {
-            source: "assets/route-optimization.jpg",
+            source: "/assets/route-optimization.jpg",
             alt: "searching places",
             width: 586,
             height: 494
@@ -139,7 +139,7 @@ export const features: Array<Features> = [
         subtitle: "ONM",
         description: "API one-to-many routing feature allows you to easily generate optimized routes from a single point to multiple destinations. Whether managing deliveries, planning logistics, or mapping out multiple stops, our API ensures that your users receive the most efficient paths to all their endpoints in one go",
         image: {
-            source: "assets/onm.jpg",
+            source: "/assets/onm.jpg",
             alt: "searching places",
             width: 586,
             height: 494
