9a7fc69a140539f1adab18ed79ad06ed9946532e
zubeyranwar <zubeyranwar@gmail.com>
2025-08-28 14:31:35 +0300

    chore: Remove all configuration

--- src/utils/getBlurDataURL.ts
+++ src/utils/getBlurDataURL.ts
@@ -4,12 +4,7 @@ import fs from "node:fs/promises";
 export const getBlurData = async (src: string) => {
     try {
         const buffer = await fs.readFile(`./public${src}`);
-        const {base64} = await getPlaiceholder(buffer, {
-            brightness: 0.2,
-            saturation: 1.2,
-            size: 10,
-            removeAlpha: false
-        });
+        const {base64} = await getPlaiceholder(buffer);
         return base64;
     } catch (error) {
         console.error("Error generating blur data:", error);
