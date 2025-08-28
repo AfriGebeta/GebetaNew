85ea763bd358cdd8ae0f673608a39e9b62f06f8e
zubeyranwar <zubeyranwar@gmail.com>
2025-08-28 23:45:20 +0300

    feat: Make footer sticky

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/app/(marketing)/Navbar";
 import Footer from "@/app/(marketing)/Footer";
 import React from "react";
+import LenisScrollProvider from "@/providers/lenis-provider";
 
 
 export default function RootLayout({
@@ -9,7 +10,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <>
+        <LenisScrollProvider>
             <div
                 className="relative w-full antialiased pt-[50px] flex flex-col min-h-screen dark:bg-[#05050a]"
             >
@@ -19,6 +20,6 @@ export default function RootLayout({
                 </div>
                 <Footer/>
             </div>
-        </>
+        </LenisScrollProvider>
     );
 }
