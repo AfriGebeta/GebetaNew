6f46ae17906a3b4b4fcad99ffde87e3d3dd1f691
zubeyranwar <zubeyranwar@gmail.com>
2026-01-19 09:12:24 +0300

    feat: Add piza hut and belay partners and change the title

--- src/sections/Partners.tsx
+++ src/sections/Partners.tsx
@@ -5,9 +5,7 @@ export default function Partners() {
     return (
         <Container>
             <div className="flex flex-col justify-center items-center mt-[98px]">
-                <p className="text-[12px] text-[#979BAA] tracking-20 leading-15 font-bold uppercase text-center">Location
-                    solutions
-                    powering innovative businesses</p>
+                <p className="text-[12px] text-[#979BAA] tracking-20 leading-15 font-bold uppercase text-center">Clients and Partners</p>
                 <div className="w-full flex gap-[32px] md:justify-between flex-wrap items-center mt-[40px]">
                     <div className="fade-x h-[160px] w-full overflow-hidden opacity-100 dark:opacity-1 dark:grayscale">
                         <div className="animate-scroll-x h-full w-full bg-[length:200%_auto] md:bg-[length:110%_auto]"
