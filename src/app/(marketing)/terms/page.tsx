842061bb6d4d8e9b150e957038c3eb71a1bc51df
abenezer121 <webgoat12@gmail.com>
2026-04-03 08:51:48 +0300

    [fix]- spacing issue for terms

--- src/app/(marketing)/terms/page.tsx
+++ src/app/(marketing)/terms/page.tsx
@@ -48,9 +48,16 @@ export default async function Terms() {
 
     return (
         <Container>
-            <article className="mt-[20px] prose prose-lg prose-gray max-w-none
-                          prose-headings:font-bold prose-a:text-blue-600
-                          prose-strong:text-gray-900">
+
+            <article
+                className="mt-[120px] prose prose-lg max-w-none
+             prose-headings:font-bold
+             prose-h1:text-4xl prose-h2:text-3xl
+             prose-h1:mb-4 prose-h2:mb-3
+             prose-p:mt-2
+             prose-p:text-gray-700
+             [&>*:first-child]:mt-0"
+            >
                 <MDXRemote source={data.content} />
             </article>
         </Container>
