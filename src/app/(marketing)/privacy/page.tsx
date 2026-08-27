5bca328d8199f090b4052137de3703e3ab5bf5ac
abenezer121 <webgoat12@gmail.com>
2026-08-27 21:21:10 +0300

    Force dynamic rendering on privacy page

--- src/app/(marketing)/privacy/page.tsx
+++ src/app/(marketing)/privacy/page.tsx
@@ -5,6 +5,8 @@ export const metadata:Metadata = {
     title:"Privacy Policy"
 }
 
+export const dynamic = 'force-dynamic';
+
 export default function PrivacyPolicy() {
     return (
         <Container>
